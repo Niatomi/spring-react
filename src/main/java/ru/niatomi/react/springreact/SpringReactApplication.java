36e2bff8e71a8ec0a1ddbf93fc2dfd996d0202dc
Niatomi <playervoker@gmail.com>
2022-05-28 23:48:06 +0400

    Initial commit

---
+++ src/main/java/ru/niatomi/react/springreact/SpringReactApplication.java
@@ -0,0 +1,13 @@
+package ru.niatomi.react.springreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactApplication.class, args);
+	}
+
+}
